303bede9308641d865c5717190d2e241cdb9643b
Angie <angelicarlos@icloud.com>
2023-03-23 16:55:42 +1100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+
+    <BrowserRouter>
+    
+      <div className="App">
+
+        <Routes>
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/articles" element={<ArticlesList />} />
+        <Route path="/articles/:articleId" element={<ArticlePage />} />
+
+        </Routes>
+        
+      </div>
+    
+    </BrowserRouter>
+    
   );
 }
 
