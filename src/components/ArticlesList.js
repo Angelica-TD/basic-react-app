36ef0c3424886b963dfe55b80f0786d4161d1867
Angie <angelicarlos@icloud.com>
2023-03-23 21:28:31 +1100

    make articles list component for reusability

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const ArticlesList = ({ articles }) => {
+    return (
+        <>
+            {articles.map(article => (
+                <div key={article.name}>
+                    <Link to={`/articles/${article.name}`}>
+                        <h3>{article.title}</h3>
+                    </Link>
+                    <p>{article.content[0].substring(0, 150)}...</p>
+                </div>
+            ))}
+        </>
+
+    )
+}
+
+export default ArticlesList;
