a703e18b7fdbf8d1f947245ee5b940237dfc4cbd
Angie <angelicarlos@icloud.com>
2023-03-25 18:10:02 +1100

    integrate backend

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import articles from './article-content';
+import axios from "axios";
+import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
+
 
 const ArticlePage = () =>{
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const { articleId } = useParams();
 
-    const params = useParams();
-    const articleId = params.articleId;
+    useEffect(()=>{
+
+        const loadArticleInfo = async()=>{
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        loadArticleInfo();
+        
+    }, []);
+
     const article = articles.find(article => article.name === articleId);
 
     if (!article){
@@ -15,10 +31,11 @@ const ArticlePage = () =>{
     return(
         <>
         <h1>{article.title}</h1>
-
+        <p>This article has {articleInfo.pageViews} view(s)</p>
         {article.content.map((paragraph, index) => (
             <p key={index}>{ paragraph }</p>
         ))}
+        <CommentsList comments={articleInfo.comments} />
         </>
     )
 }
